8f89df512231ca9ad33a44042f06d55d68adaebd
tuankg1028 <tuan10281028@gmail.com>
2018-03-10 20:58:16 +0700

    xong

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 /* script product-detail */
+	/* Thay background-image cho idZoom */
+	var link_img_zoom = document.getElementById("imgZoom").getAttribute('src');
+	document.getElementById("overlay").style.backgroundImage = 'url('+link_img_zoom+')';
 	/* Thay đổi hình anh khi click vào hình nhỏ */
 	var img_items = document.querySelectorAll("#product-detail .describe .left .gallery-img .wrap ul li.item img");
 	for (var i=0; i < img_items.length; i++) {
@@ -15,6 +18,10 @@
 		}
 		/* thêm class active */
     	this.parentElement.classList.add("active");
+
+    	/* Thay background-image cho idZoom */
+    	var link_img_zoom = document.getElementById("imgZoom").getAttribute('src');
+		document.getElementById("overlay").style.backgroundImage = 'url('+link_img_zoom+')';
     }
     /* end Thay đổi hình anh khi click vào hình nhỏ */
 
@@ -27,7 +34,7 @@
 	  var posX = event.offsetX ? (event.offsetX) : event.pageX - img.offsetLeft;
 	  var posY = event.offsetY ? (event.offsetY) : event.pageY - img.offsetTop;
 
-	  console.log(posX + '-' + posY);
+	  element.style.backgroundSize = "200% 200%";
 	  element.style.backgroundPosition = (-posX * 0.8) + "px " + (-posY * 0.8) + "px";
 
 	}
